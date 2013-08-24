8d50db3e1cbd5ced47331d4769e94f58f0df4f39
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2013-08-24 20:18:37 +0530

    Add forbetterenglish.com as a source

--- scrap_it.rb
+++ scrap_it.rb
@@ -6,14 +6,22 @@ puts "###### 1"
 
 doc = Nokogiri::HTML(open('http://www.reference.com/example-sentences/abjure'))
 
-doc.xpath('//*[@id="sentence_examples"]/div[*]').each do |link|
-  puts link.content unless link.content == "more"
+doc.xpath('//*[@id="sentence_examples"]/div[*]').each do |div|
+  puts div.content unless div.content == "more"
 end
 
 puts "###### 2"
 
 doc = Nokogiri::HTML(open('http://sentence.yourdictionary.com/voracious'))
 
-doc.xpath('/html/body/div[*]/div[*]/div[*]/section[*]/div/ul/li[*]').each do |link|
-  puts link.content
+doc.xpath('/html/body/div[*]/div[*]/div[*]/section[*]/div/ul/li[*]').each do |li|
+  puts li.content	
+end
+
+puts "###### 3"
+
+doc = Nokogiri::HTML(open('http://forbetterenglish.com/index.cgi?page=acd&article=an&language=English&str=dispose'))
+
+doc.xpath('//tr//td[3]').each do |td|
+  puts td.text.strip.gsub("\n", ' ').squeeze(' ').gsub(" .", '.')
 end
